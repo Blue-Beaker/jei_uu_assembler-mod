cca2cdacf89cb93fc0eaa83f0c8628eca1b58d68
blue-beaker <yyy5566778899@126.com>
2025-10-15 16:45:13 +0800

    Add ashes to solid heater category

--- src/main/java/io/bluebeaker/jei_uu_assembler/jei/generator/SolidHeaterCategory.java
+++ src/main/java/io/bluebeaker/jei_uu_assembler/jei/generator/SolidHeaterCategory.java
@@ -2,13 +2,16 @@ package io.bluebeaker.jei_uu_assembler.jei.generator;
 
 import ic2.core.block.heatgenerator.tileentity.TileEntitySolidHeatGenerator;
 import ic2.core.init.MainConfig;
+import ic2.core.item.type.MiscResourceType;
 import ic2.core.ref.BlockName;
+import ic2.core.ref.ItemName;
 import ic2.core.ref.TeBlock;
 import ic2.core.util.ConfigUtil;
 import io.bluebeaker.jei_uu_assembler.jei.generic.FuelRecipeWrapper;
 import io.bluebeaker.jei_uu_assembler.jei.generic.GenericRecipeCategory;
 import io.bluebeaker.jei_uu_assembler.utils.EnergyUnit;
 import io.bluebeaker.jei_uu_assembler.utils.IC2Drawables;
+import io.bluebeaker.jei_uu_assembler.utils.RenderUtils;
 import io.bluebeaker.jei_uu_assembler.utils.Utils;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import mezz.jei.api.IGuiHelper;
@@ -17,9 +20,11 @@ import mezz.jei.api.gui.IDrawableStatic;
 import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.ingredients.VanillaTypes;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -40,7 +45,9 @@ public class SolidHeaterCategory extends GenericRecipeCategory<SolidHeaterCatego
     public void setRecipe(IRecipeLayout recipeLayout, Wrapper wrapper, IIngredients iIngredients) {
         IGuiItemStackGroup guiItemStackGroup = recipeLayout.getItemStacks();
         this.addItemSlot(guiItemStackGroup,0,8,GUI_HEIGHT/2);
+        this.addItemSlot(guiItemStackGroup,1,GUI_WIDTH-26,GUI_HEIGHT/2-9);
         guiItemStackGroup.set(iIngredients);
+        guiItemStackGroup.set(1,Wrapper.ashes);
     }
 
     @Override
@@ -68,9 +75,24 @@ public class SolidHeaterCategory extends GenericRecipeCategory<SolidHeaterCatego
 
     public static class Wrapper extends FuelRecipeWrapper {
         public static int Generation = TileEntitySolidHeatGenerator.emittedHU;
+        public static final ItemStack ashes = ItemName.misc_resource.getItemStack(MiscResourceType.ashes);
         public Wrapper(List<ItemStack> input, int duration) {
             super(input, Generation, duration);
         }
+
+        @Override
+        public void getIngredients(IIngredients iIngredients) {
+            super.getIngredients(iIngredients);
+            iIngredients.setOutput(VanillaTypes.ITEM, ashes);
+        }
+
+        @Override
+        public void drawInfo(Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
+            super.drawInfo(minecraft, recipeWidth, recipeHeight, mouseX, mouseY);
+
+            RenderUtils.drawTextAlignedMiddle("50%", recipeWidth-17, recipeHeight/2+10, Color.gray.getRGB());
+        }
+
         @Override
         public String getPowerUnit() {
             return EnergyUnit.HU.name;
